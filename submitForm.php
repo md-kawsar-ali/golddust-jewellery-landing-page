702e777f42c2e216dae54f84ae4f2a1c144ab7c4
mdkawsarali <facebookerjordan@gmail.com>
2022-07-15 18:32:53 +0600

    Responsive Done

--- submitForm.php
+++ submitForm.php
@@ -5,18 +5,18 @@ if(isset($_POST['name']) && isset($_POST['email'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
-    $business = $_POST['business'];
+    $message = $_POST['message'];
     $subject = 'New Message from Become Partner Form!';
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From: admin@tipsu.eu' . PHP_EOL .
+    $headers .= 'From: admin@golddust.com' . PHP_EOL .
     'Reply-To:' . $email . PHP_EOL .
     'X-Mailer: PHP/' . phpversion();
     
-    $to = 'info@tipsu.eu'; 
+    $to = 'info@golddust.com'; 
     
-    $messageSent = '<div>Name : '. $name .'</div><div> Email : '. $email .' </div><div>Phone : '. $phone .'</div><div> Business : '. $business .'</div>';
+    $messageSent = '<div>Name : '. $name .'</div><div> Email : '. $email .' </div><div>Phone : '. $phone .'</div><div> Message : '. $message .'</div>';
 
     mail($to, $subject, $messageSent, $headers);
     echo 'Thank you, We will get back to you asap!';
